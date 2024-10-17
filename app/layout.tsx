f0e58116f4d063f236ef523278f73538e20b8664
Alejandro Campos <acamposcar@gmail.com>
2024-10-17 21:42:54 +0200

    x

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,35 @@
-import './globals.css';
-import type { Metadata, Viewport } from 'next';
-import { Manrope } from 'next/font/google';
-import { UserProvider } from '@/lib/auth';
-import { getUser } from '@/lib/db/queries';
+import "./globals.css";
+import type { Metadata, Viewport } from "next";
+import { Manrope } from "next/font/google";
+import { UserProvider } from "@/lib/auth";
+import { getUser } from "@/lib/db/queries";
 
 export const metadata: Metadata = {
-  title: 'Bookinger',
-  description: 'Youtube to Booking.',
+	title: "Bookinger VTS",
+	description: "Book technology.",
 };
 
 export const viewport: Viewport = {
-  maximumScale: 1,
+	maximumScale: 1,
 };
 
-const manrope = Manrope({ subsets: ['latin'] });
+const manrope = Manrope({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  const userPromise = getUser();
+	const userPromise = getUser();
 
-  return (
-    <html
-      lang="en"
-      className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
-
-    >
-      <body className="min-h-[100dvh] bg-gray-50 dark:bg-gray-900">
-        <UserProvider userPromise={userPromise}>{children}</UserProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html
+			lang="en"
+			className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
+		>
+			<body className="min-h-[100dvh] bg-gray-50 dark:bg-gray-900">
+				<UserProvider userPromise={userPromise}>{children}</UserProvider>
+			</body>
+		</html>
+	);
 }
